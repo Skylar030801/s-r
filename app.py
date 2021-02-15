b555c14314b8bbd68a0e38914ff331328d9f4c74
skylarH <elantrahufkie@gmail.com>
2021-02-15 15:01:11 +0200

    first commit

--- app.py
+++ app.py
@@ -1,36 +1,87 @@
 import sqlite3
-from flask import Flask, render_template,request
+from flask import Flask, render_template, request
+
 
 def init_sqlite_db():
- conn = sqlite3.connect('database.db')
- print("Opened database successfully")
- conn.execute('CREATE TABLE IF NOT EXISTS students (name TEXT, addr TEXT, city TEXT, pin TEXT)')
- print("Table created successfully")
- conn.close()
+
+    conn = sqlite3.connect('database.db')
+    print("Opened database successfully")
+
+    conn.execute('CREATE TABLE IF NOT EXISTS student (id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT, address TEXT, city TEXT, pin_code TEXT)')
+    print("Table created successfully")
+    conn.close()
+
+
 init_sqlite_db()
+
+
 app = Flask(__name__)
-@app.route('/enter-new/')
+
+@app.route('/')
+@app.route('/registration-form/')
 def enter_new_student():
- return render_template('student.html')
+    return render_template('student.html')
+
 
 @app.route('/add-new-record/', methods=['POST'])
 def add_new_record():
- if request.method == "POST":
-     try:
-        name = request.form['name']
-        addr = request.form['add']
-        city = request.form['city']
-        pin = request.form['pin']
+    if request.method == "POST":
+        msg = None
+        try:
+            name = request.form['name']
+            address = request.form['address']
+            city = request.form['city']
+            pin_code = request.form['pin-code']
 
+            with sqlite3.connect('database.db') as con:
+                cur = con.cursor()
+                cur.execute("INSERT INTO student (name, address, city, pin_code) VALUES (?, ?, ?, ?)", (name, address, city, pin_code))
+                con.commit()
+                msg = name + " was successfully added to the database."
+        except Exception as e:
+            con.rollback()
+            msg = "Error occurred in insert operation: " + str(e)
+
+        finally:
+            con.close()
+            return render_template('results.html', msg=msg)
+
+
+@app.route('/show-records/', methods=["GET"])
+def show_records():
+    records = []
+    try:
         with sqlite3.connect('database.db') as con:
-          cur = con.cursor()
-          cur.execute("INSERT INTO students (name, addr, city, pin) VALUES (?, ?, ?, ?)", (name, addr, city, pin))
-          con.commit()
-          msg = "Record successfully added."
-     except Exception as e:
-          con.rollback()
-          msg = "Error occurred in insert operation: " + e
+            cur = con.cursor()
+            cur.execute("SELECT * FROM student")
+            records = cur.fetchall()
+    except Exception as e:
+        con.rollback()
+        print("There was an error fetching results from the database: " + str(e))
+    finally:
+        con.close()
+        return render_template('records.html', records=records)
 
-     finally:
-          con.close()
-          return render_template('result.html', msg=msg)
+
+@app.route('/delete-student/<int:student_id>/', methods=["GET"])
+def delete_student(student_id):
+
+    msg = None
+    try:
+        with sqlite3.connect('database.db') as con:
+            cur = con.cursor()
+            cur.execute("DELETE FROM student WHERE id=" + str(student_id))
+            con.commit()
+            msg = "A record was deleted successfully from the database."
+    except Exception as e:
+        con.rollback()
+        msg = "Error occurred when deleting a student in the database: " + str(e)
+    finally:
+        con.close()
+        return render_template('delete-success.html', msg=msg)
+
+
+if __name__=="__main__":
+    app.run()
+
+
